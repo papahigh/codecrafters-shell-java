cc77a5027ac751642ea83ef7ac416d804e443197
Nikola P <nikolay.papakha@gmail.com>
2025-01-10 19:02:35 +0300

    add repl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        System.out.print("$ ");
+        while (true) {
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
 
-        System.out.println(input + ": command not found");
+            System.out.print("$ ");
+
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            System.out.println(input + ": command not found");
+
+        }
     }
 }
