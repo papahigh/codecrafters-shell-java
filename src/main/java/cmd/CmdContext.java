2f36603a547a73468b1316e0b16754af251eb963
Nikola P <nikolay.papakha@gmail.com>
2025-01-10 22:19:18 +0300

    Single quotes

--- src/main/java/cmd/CmdContext.java
+++ src/main/java/cmd/CmdContext.java
@@ -1,6 +1,9 @@
 package cmd;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public record CmdContext(String input, String[] args) {
 
@@ -8,13 +11,8 @@ public record CmdContext(String input, String[] args) {
         return args[0];
     }
 
-    public String argsAsString() {
-        return input.substring(command().length() + 1);
-    }
-
-    public Iterable<String> iterator(int from) {
-        return () -> new Iterator<>() {
-
+    public Iterator<String> argsIterator(int from) {
+        return new Iterator<>() {
             private int index = from;
 
             @Override
@@ -29,6 +27,10 @@ public record CmdContext(String input, String[] args) {
         };
     }
 
+    public String argsAsString() {
+        return input.substring(command().length() + 1);
+    }
+
     public String arg(int i) {
         return args[i];
     }
@@ -37,11 +39,59 @@ public record CmdContext(String input, String[] args) {
         return Integer.parseInt(args[i]);
     }
 
-    public boolean isBlank() {
-        return input.isBlank();
+    public static CmdContext of(String input) {
+        return new CmdContext(input, new Parser().parse(input));
     }
 
-    public static CmdContext of(String input) {
-        return new CmdContext(input, input.split("\\s+"));
+    static class Parser {
+
+        private final List<String> args = new ArrayList<>();
+
+        private State state = State.DEFAULT_PARSE;
+        private StringBuilder sb = new StringBuilder();
+
+        private enum State {
+            DEFAULT_PARSE,
+            SINGLE_QUOTE
+        }
+
+        String[] parse(String input) {
+            for (char c : input.toCharArray())
+                consume(c);
+            flush();
+            return args.toArray(String[]::new);
+        }
+
+        private void consume(char c) {
+            if (Character.isWhitespace(c)) {
+                if (state == State.SINGLE_QUOTE)
+                    sb.append(c);
+                else
+                    flush();
+            } else if (c == '\'') {
+                if (state == State.SINGLE_QUOTE) {
+                    state = State.DEFAULT_PARSE;
+                    flush();
+                } else {
+                    state = State.SINGLE_QUOTE;
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+
+        private void flush() {
+            if (!sb.isEmpty()) {
+                if (state == State.SINGLE_QUOTE) {
+                    sb.insert(0, '\'');
+                    args.addAll(Arrays.asList(sb.toString().split("\\s+")));
+                } else {
+                    args.add(sb.toString());
+                }
+                sb = new StringBuilder();
+            }
+            state = State.DEFAULT_PARSE;
+        }
     }
+
 }
--- src/main/java/cmd/Executable.java
+++ src/main/java/cmd/Executable.java
@@ -108,7 +108,12 @@ public final class Executable {
     static final class EchoCmd implements Cmd {
         @Override
         public void execute(CmdContext context) {
-            System.out.println(context.argsAsString());
+            var it = context.argsIterator(1);
+            while (it.hasNext()) {
+                System.out.print(it.next());
+                if (it.hasNext()) System.out.print(' ');
+            }
+            System.out.println();
         }
     }
 
