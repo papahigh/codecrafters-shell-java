b0cfee563e639e6068d8a0f91b8bb840d3741628
Nikola P <nikolay.papakha@gmail.com>
2025-01-10 19:46:34 +0300

    fix type builtin

--- src/main/java/cmd/TypeCmd.java
+++ src/main/java/cmd/TypeCmd.java
@@ -4,11 +4,11 @@ public final class TypeCmd implements Cmd {
 
     @Override
     public void execute(CmdContext context) {
-        boolean hasCmd = CmdRegistry.hasCmd(context.command());
+        boolean hasCmd = CmdRegistry.hasCmd(context.argument(1));
         if (hasCmd) {
             System.out.printf("%s is a shell builtin%n", context.argument(1));
         } else {
-            System.out.printf("%s: not found%n", context.command());
+            System.out.printf("%s: not found%n", context.argument(1));
         }
     }
 }
